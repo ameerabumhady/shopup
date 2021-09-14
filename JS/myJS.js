e52b4c00ca393ecb47000e706b54a3de217c8077
Ameer Abu Mhady <aabumhady@smail.ucas.edu.ps>
2021-09-14 13:15:29 -0700

    this commit for add my project from my PC

---
+++ JS/myJS.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+{
+	$(document).on('click','.list-down-btn',function(event){
+  	event.preventDefault();
+    var target = $(this).attr('data-toggle');
+    var ss = $('.sub-titel-cat');
+    
+    $(target).slideToggle();
+    var clicked = event.target;
+    $(clicked).toggleClass("glyphicon-chevron-down  glyphicon-chevron-up");
+    $(this).$(ss).css('color', 'red');
+  });
+
+  $(".show-modal").click(function(){
+    $(".modal").show();
+  });
+
+  // $(document).on('click','.dropdown',function(event){
+  // 	event.preventDefault();
+  //   var target = $(this).attr('data-toggle');
+  //   var ss = $('.back-color-red');
+    
+  //   $(target).slideToggle();
+  //   var clicked = event.target;
+  //   // $(clicked).toggleClass("glyphicon-chevron-down  glyphicon-chevron-up");
+  //   $(this).$(ss).css('background-color', 'red');
+  // });
+
+  // $(".dropdown").click(function(){
+  //   $(this).addClass("back-color-red");
+  // })
+});
